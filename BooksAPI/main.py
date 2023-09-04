d925f1005093cd29fb1d78b0f50e750dd0601fa4
PabloSsolbal <pablossolbal@gmail.com>
2023-09-04 08:43:00 -0600

    system v1

---
+++ BooksAPI/main.py
@@ -0,0 +1,84 @@
+# ! This code creates a API to get data from a Books DB -personal DB-
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+# ? Import the FastAPI librarys to create the views and manage the CORS
+# ? Import the pymongo client and serverAPI to connect to the database
+
+# ! Start the App with FastAPI
+app = FastAPI()
+
+# ? the database string to connect to the db
+MONGO_URL = "mongodb+srv://PabloSSolbal:Pablopicotazo@cluster0.n8i8xdz.mongodb.net/?retryWrites=true&w=majority"
+
+# ? create a client to comunicate to the db
+client = MongoClient(MONGO_URL, server_api=ServerApi('1'))
+
+# ! Allows CORS from all
+origins = ["*"]
+
+# ? Set the CORS Deatils
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ! Views Section
+
+# ? This function returns my books wishlist.
+# * Create an empty list to store the books in the wishlist.
+# * Iterate through all the books in the library.
+# * Add the book to the wishlist.
+# * Return the wishlist.
+
+
+@app.get("/wishlist")
+async def root():
+
+    wishlist = []
+
+    for book in client.Library.Books.find({"wishList": True}):
+        wishlist.append({"name": book['name'], "author": book['author']})
+
+    return wishlist
+
+# ? This function returns my library.
+# * Create an empty list to store the books in the library.
+# * Iterate through all the books in the library.
+# * Add the book to the library, along with the book's read status.
+# * Return the library.
+
+
+@app.get("/library")
+async def root():
+
+    library = []
+
+    for book in client.Library.Books.find({"wishList": False}):
+        library.append(
+            {"name": book['name'], "author": book['author'], "isReaded": book['readed']})
+
+    return library
+
+# ! view not used currently
+# ? This function returns all the books by the specified author.
+# * Create an empty list to store the books.
+# * Iterate through all the books in the library.
+# * Add the book to the list.
+# * Return the list of books.
+
+
+@app.get("/books/{author}")
+async def root(author: str):
+
+    books = []
+
+    for book in client.Library.Books.find({"author": author}):
+        books.append(
+            {"name": book['name'], "author": book['author'], "isReaded": book['readed']})
+
+    return books
---
+++ BooksFront/app.js
@@ -0,0 +1,164 @@
+/**
+ * ! Creates a list of books app from an API
+ * ? This code manages a web application for tracking books in a wishlist and library.
+ * * It dynamically fetches data from an API, displays it, and allows user to switch between
+ * *  their wishlist and library.
+ */
+
+// ? window variables to call the API
+const library = window.library;
+const wishlist = window.wishlist;
+
+// ? elements of the dom that we will use
+/**
+ * * title of the page
+ * * books container
+ * * buttons to change the content
+ */
+const title = document.querySelector(".title");
+const books = document.querySelector(".books");
+const btns = document.querySelectorAll(".btn");
+
+// ? array to save the current data
+let data = [];
+
+// ? Functions to get the data from the API
+/**
+ *  * The function are asynchronous and returns a Promise.
+ *  * Make an asynchronous HTTP request
+ *  * The response will be a JSON object. Convert it to a JavaScript object
+ *  * Store the data in the `data` variable.
+ *  * Clear the body of the page. - ClearBody function
+ *  * Create a list of books from the data. - CreateBooks function
+ */
+
+// ? Function to get the wishlist of books
+const getWishlist = async () => {
+  await fetch(wishlist)
+    .then((res) => res.json())
+    .then((res) => {
+      data = res;
+    });
+  clearBody();
+  createBooks();
+};
+
+// ? Function to get the library of books
+
+const getLibrary = async () => {
+  await fetch(library)
+    .then((res) => res.json())
+    .then((res) => {
+      data = res;
+    });
+  clearBody();
+  createBooks();
+};
+
+// ? Function to create the list of books
+/**
+ * * This function creates a list of book items from the data.
+ * * Create two empty arrays to store the read and unread books.
+ * * Iterate through the data array.
+ * * Create a new book item element.
+ * * Create two new text elements for the book name and author.
+ * * Set the text content of the book name and author elements.
+ * * Append the book name and author elements to the book item element.
+ *
+ * * If the book is not read, add it to the notReadedBooks array.
+ * * Else add it to the readedBooks array.
+ * * Combine the read and unread books arrays into a single array.
+ *
+ * * Iterate through the allBooks array and append each book item to the books container.
+ */
+const createBooks = () => {
+  const readedBooks = [];
+  const notReadedBooks = [];
+
+  data.forEach((book) => {
+    const bookItem = document.createElement("div");
+    bookItem.classList.add("book");
+
+    const bookName = document.createElement("div");
+    bookName.classList.add("name");
+
+    const bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("author");
+
+    bookName.textContent = book.name;
+    bookAuthor.textContent = book.author;
+
+    bookItem.appendChild(bookName);
+    bookItem.appendChild(bookAuthor);
+
+    if (book.isReaded === false) {
+      bookItem.classList.add("not-readed");
+      notReadedBooks.push(bookItem);
+    } else {
+      readedBooks.push(bookItem);
+    }
+  });
+
+  const allBooks = readedBooks.concat(notReadedBooks);
+
+  allBooks.forEach((bookItem) => {
+    books.appendChild(bookItem);
+  });
+};
+
+// ? This function is a starter function that determines which function to call based on the parameter.
+/**
+ * * Check the parameter
+ * * Change the title of the page.
+ * * Call the function that corresponds to the parameter.
+ */
+
+const starter = (func) => {
+  if (func === "wishlist") {
+    title.textContent = "Wishlist";
+    getWishlist();
+  } else if (func === "library") {
+    title.textContent = "Library";
+    getLibrary();
+  }
+};
+
+// ? This function is a wrapper function that calls the starter function with the specified content.
+/**
+ * * Check the parameter
+ * * Calls the starter with the func based on parameter
+ */
+
+const call = (content) => {
+  if (content === "Wishlist") {
+    starter("wishlist");
+  } else if (content === "Library") {
+    starter("library");
+  }
+};
+
+// ? Clear the html function -To blank the page before charge the books-
+const clearBody = () => {
+  books.innerHTML = "";
+};
+
+// ? This function iterates through the btns array and adds an event listener to each button.
+/**
+ * * Add an event listener to the button.
+ * * Remove the "select" class from all buttons.
+ * * Add the "select" class to the clicked button.
+ * * Call the call function with the text content of the clicked button.
+ */
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btns.forEach((btn) => {
+      btn.classList.remove("select");
+    });
+    btn.classList.add("select");
+
+    call(btn.textContent);
+  });
+});
+
+// ? start the app with the wishlist content
+starter("wishlist");
