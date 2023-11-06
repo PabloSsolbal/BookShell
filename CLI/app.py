08c7c87652452383c53f5c277f4bead90e15d325
PabloSsolbal <pablossolbal@gmail.com>
2023-11-06 10:51:33 -0600

    CLI code commented

--- CLI/app.py
+++ CLI/app.py
@@ -3,6 +3,14 @@ import sys
 
 
 class Library:
+    """
+        * The Library class provides a command-line interface for interacting with a book library using the 'db_manager'.
+        * It allows users to perform various operations on books such as retrieval, creation, deletion, and more.
+    """
+    """
+        * This is important because we need the db_manager
+        * Allow to import the db_manager without errors
+    """
     db_manager = None
     sys.path.append("../BooksAPI")
     import db_manager
@@ -10,27 +18,52 @@ class Library:
 
     @classmethod
     def get_book_name(cls) -> str:
+        """
+            ? Prompt the user to enter a book name and return it as a string.
+
+            Returns:
+                * - str: The book name entered by the user.
+        """
         name = input("Enter book name: ")
         return name
 
     @classmethod
     def create_book_dict(cls) -> dict:
+        """
+            ? Prompt the user to enter book details and return them as a dictionary.
+
+            Returns:
+                * - dict: A dictionary containing book details including name, author, readed, and wishList.
+        """
         name = input("Enter book name: ")
         author = input("Enter author name: ")
         readed = input("Is readed (y/n): ")
+
         if readed == "y" or readed == "Y":
             wishlist = "n"
         else:
             wishlist = input("Is in wishlist (y/n): ")
+
         return {"name": name, "author": author, "readed": True if readed == "y" or readed == "Y" else False, "wishList": True if wishlist == "y" or wishlist == "Y" else False}
 
     @classmethod
     def print_books(cls, books: list | dict):
+        """
+            ? Display a list of books or a message to the console.
+
+            Args:
+                * - books (list | dict): Either a list of books or a dictionary containing a message.
+
+            Notes:
+                * - If 'message' is present in the dictionary, it will print the message.
+                * - If the input is a dictionary, it will be converted to a list for printing if needed.
+        """
         if 'message' in books:
             print(books["message"])
         else:
             if type(books) == dict:
                 books = [books]
+
             for book in books:
                 if type(book) == dict:
                     book = f"{book['name']} - {book['author']} - {'readed' if book['isReaded']==True else 'unreaded'}" if 'isReaded' in book else f"{book['name']} - {book['author']}"
@@ -40,90 +73,157 @@ class Library:
 
     @classmethod
     def get_books(cls):
+        """
+            * Retrieve and print a list of all books in the library.
+        """
         books = cls.db_manager.get_books()
         Library.print_books(books)
 
     @classmethod
     def get_book(cls):
+        """
+            * Retrieve and print a book by name.
+        """
         name = Library.get_book_name()
         books = cls.db_manager.get_book(name)
         Library.print_books(books)
 
     @classmethod
     def get_readed_books(cls):
+        """
+            * Retrieve and print a list of readed books.
+        """
         books = cls.db_manager.get_readed_books()
         Library.print_books(books)
 
     @classmethod
     def get_books_to_read(cls):
+        """
+            * Retrieve and print a list of books to read (unreaded books).
+        """
         books = cls.db_manager.get_books_to_read()
         Library.print_books(books)
 
     @classmethod
     def get_authors(cls):
+        """
+            * Retrieve and print a list of unique authors from the library.
+        """
         books = cls.db_manager.get_authors()
         Library.print_books(books)
 
     @classmethod
     def get_books_in_library(cls):
+        """
+            * Retrieve and print a list of books in the library (excluding wishlist books).
+        """
         books = cls.db_manager.get_books_in_library()
         Library.print_books(books)
 
     @classmethod
     def get_books_in_wishlist(cls):
+        """
+            * Retrieve and print a list of books in the wishlist.
+        """
         books = cls.db_manager.get_books_in_wishlist()
         Library.print_books(books)
 
     @classmethod
     def get_favorites(cls):
+        """
+            * Retrieve and print a list of favorite books.
+        """
         books = cls.db_manager.get_favorites()
         Library.print_books(books)
 
     @classmethod
     def get_books_by_author(cls):
+        """
+            * Prompt the user to enter an author's name, then retrieve and print books by that author.
+        """
         author = input("Enter author name: ")
         books = cls.db_manager.get_books_by_author(author)
         Library.print_books(books)
 
     @classmethod
     def create_book(cls):
+        """
+            * Prompt the user to enter book details, create a new book entry, and print the result.
+        """
         book = Library.create_book_dict()
         result = cls.db_manager.create_book(book)
         Library.print_books(result)
 
     @classmethod
     def delete_book(cls):
+        """
+            * Prompt the user to enter a book name, delete the book, and print the result.
+        """
         name = Library.get_book_name()
         result = cls.db_manager.delete_book(name)
         Library.print_books(result)
 
     @classmethod
     def read_book(cls):
+        """
+            * Prompt the user to enter a book name, mark the book as read, and print the result.
+        """
         name = Library.get_book_name()
         result = cls.db_manager.read_a_book(name)
         Library.print_books(result)
 
     @classmethod
     def book_out_of_wishlist(cls):
+        """
+            * Prompt the user to enter a book name, remove the book from the wishlist, and print the result.
+        """
         name = Library.get_book_name()
         result = cls.db_manager.out_of_wishlist(name)
         Library.print_books(result)
 
     @classmethod
     def add_book_to_favorites(cls):
+        """
+            * Prompt the user to enter a book name, mark the book as a favorite, and print the result.
+        """
         name = Library.get_book_name()
         result = cls.db_manager.add_to_favorites(name)
         Library.print_books(result)
 
 
 class CLI:
+    """
+        ? Command Line Interface (CLI) for interacting with the book library.
+
+        * This class provides a command-line interface to perform various operations on a book library.
+        * It includes methods for displaying a menu, receiving user commands, and executing operations.
+
+        Methods:
+            * - clear_screen: Clears the terminal screen.
+            * - show_menu: Displays the available commands in the menu.
+            * - get_command: Prompts the user for a command and returns it as a string.
+            * - execute: Runs the CLI interface and processes user commands to interact with the library.
+
+        Example Usage:
+            To use the CLI, create an instance of the `CLI` class and call the `execute` method to start the interaction.
+    """
 
     @classmethod
     def clear_screen(cls):
+        """
+            ? Clears the terminal screen.
+
+            * This method detects the operating system and uses the appropriate command to clear the screen.
+        """
         os.system('cls' if os.name == 'nt' else 'clear')
 
     @classmethod
     def show_menu(cls):
+        """
+            ? Displays the available commands in the menu.
+
+            * This method prints a list of available commands that the user can enter to interact with the library.
+        """
         print("Commands:")
         print("--------------------")
         print("all - show all books")
@@ -145,11 +245,23 @@ class CLI:
 
     @classmethod
     def get_command(cls) -> str:
+        """
+            ? Prompts the user for a command and returns it as a string.
+
+            Returns:
+                * - str: The user-entered command.
+        """
         command = input("Enter command: ")
         return command.strip()
 
     @classmethod
     def execute(cls):
+        """
+            ? Runs the CLI interface and processes user commands to interact with the library.
+
+            * This method continuously prompts the user for commands and executes the corresponding library operations.
+            * It provides an interactive way to manage the book library based on user input.
+        """
         while True:
             input()
             cls.clear_screen()
@@ -206,6 +318,9 @@ class CLI:
 
 
 def main():
+    """
+        * main function to execute the program calling CLI and the method execute
+    """
     CLI.execute()
 
 
